1a828ae4a8ebab4ba66730e8829604710920be11
al-chang <aac6692@gmail.com>
2023-03-23 12:35:49 -0400

    Finish lab

--- src/tuiter/post-summary-list/post-summary-item.js
+++ src/tuiter/post-summary-list/post-summary-item.js
@@ -12,17 +12,17 @@ const PostSummaryItem = ({
   },
 }) => {
   return (
-    <li class="list-group-item d-flex justify-content-between">
-      <div class="d-flex flex-column">
-        <p class="m-0 text-secondary">{post.topic}</p>
-        <p class="fw-bold m-0">
+    <li className="list-group-item d-flex justify-content-between">
+      <div className="d-flex flex-column">
+        <p className="m-0 text-secondary">{post.topic}</p>
+        <p className="fw-bold m-0">
           {post.userName}
-          <i class="fas fa-check-circle"></i>
-          <span class="fw-normal text-secondary">- {post.time}</span>
+          <i className="fas fa-check-circle"></i>
+          <span className="fw-normal text-secondary">- {post.time}</span>
         </p>
-        <p class="fw-bold m-0">{post.title}</p>
+        <p className="fw-bold m-0">{post.title}</p>
       </div>
-      <img src={post.image} class="wd-tweet-image rounded" alt="tweet" />
+      <img src={post.image} className="wd-tweet-image rounded" alt="tweet" />
     </li>
   );
 };
